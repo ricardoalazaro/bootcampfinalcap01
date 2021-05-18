4cb92392e705be03ab354222f426c44183838c36
Ricardo Lazaro <ricardoalazaro@hotmail.com>
2021-05-18 18:59:18 -0300

    Project created and POM updated

---
+++ backend/src/main/java/com/devsuperior/trabalhofinalcap01/Trabalhofinalcap01Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabalhofinalcap01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalhofinalcap01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalhofinalcap01Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/trabalhofinalcap01/Trabalhofinalcap01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabalhofinalcap01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalhofinalcap01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
